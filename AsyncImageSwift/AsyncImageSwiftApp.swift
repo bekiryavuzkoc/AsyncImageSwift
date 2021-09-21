366dd1fb80f74c7cc25d82eb2b60aaf85933b353
Yavuz Koc <bekiryavuzkoc@icloud.com>
2021-09-21 23:12:49 +0300

    Initial Commit

---
+++ AsyncImageSwift/AsyncImageSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageSwiftApp.swift
+//  AsyncImageSwift
+//
+//  Created by Bekir Yavuz Koç on 21.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
